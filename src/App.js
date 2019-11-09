11e1afecbf7d7551ee0a41bda9c607f6e370c8b3
Aaron P <aaron4uk@gmail.com>
2019-11-09 09:37:36 -0600

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import HomeRoute from './pages/HomeRoute'
+
+function App() {
+  return (
+    <div className="App">
+      <HomeRoute />
+    </div>
+  );
+}
+
+export default App;
