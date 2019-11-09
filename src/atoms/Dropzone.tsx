ec5ecead1a8691cbfa053a64c74e0efe27c4e472
Aaron P <aaron4uk@gmail.com>
2019-11-09 17:37:22 -0600

    ts cleanup

--- src/atoms/Dropzone.tsx
+++ src/atoms/Dropzone.tsx
@@ -1,23 +1,17 @@
 import React, { useCallback } from 'react'
-import {useDropzone} from 'react-dropzone'
+import ReactDropzone from 'react-dropzone'
 
-const Dropzone = ({setFileData}) => {
+interface Props {
+  setFileData: () => null
+}
+
+const Dropzone = ({setFileData}):Props => {
   const onDrop = useCallback(acceptedFiles => {
     console.log({ acceptedFiles })
-    // setFileData
+    setFileData('hi')
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
+  return <ReactDropzone onDrop={onDrop} />
 }
 
 export default Dropzone
