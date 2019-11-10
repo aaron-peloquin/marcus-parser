fbbc77b29004456e5f221485f98e0a586bbc3f56
Aaron <aaron4uk@gmail.com>
2019-11-10 12:12:28 -0600

    build(create docs): initial commit (#2)

---
+++ docs/precache-manifest.531acb137f793c71146f2873fbcffdea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8080120a65c4b9f39033cfe923d8ee02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f41a1af3b013bb7779b",
+    "url": "./static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3d3b65bb03bcc9c0cef0",
+    "url": "./static/js/2.1d432586.chunk.js"
+  },
+  {
+    "revision": "4f41a1af3b013bb7779b",
+    "url": "./static/js/main.ace99186.chunk.js"
+  },
+  {
+    "revision": "e0f2a1c6b7945e38471c",
+    "url": "./static/js/runtime-main.57974a47.js"
+  }
+]);
