92fe2b943d4471766a4259a75cfe854950834ed5
Aaron P <aaron4uk@gmail.com>
2019-11-15 10:52:35 -0600

    adding context provider

--- src/pages/HomeRoute.tsx
+++ src/pages/HomeRoute.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import HomeLayout from './../templates/HomeLayout'
 
+export const NewRenderContext = React.createContext(() => {})
+
 const HomeRoute = () => {
-  return <HomeLayout />
+  const [numForcedRender, setNumForcedRender] = useState(0)
+  const triggerNewRender = () => {
+    const newNumberOfRenders = numForcedRender + 1
+    setNumForcedRender(newNumberOfRenders)
+  }
+
+  return <NewRenderContext.Provider value={triggerNewRender}>
+    <HomeLayout />
+  </NewRenderContext.Provider>
 }
 
 export default HomeRoute
