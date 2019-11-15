dd01fc0653108dd4c547583247354db9c73386ff
Aaron P <aaron4uk@gmail.com>
2019-11-15 11:08:49 -0600

    new build of docs

---
+++ docs/build/precache-manifest.200076e4eea4cf4b7b6bb924580f8fec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abfcc857cd6f556b205e0f72271e655",
+    "url": "./index.html"
+  },
+  {
+    "revision": "267760da0febae96b7f5",
+    "url": "./static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3d3b65bb03bcc9c0cef0",
+    "url": "./static/js/2.1d432586.chunk.js"
+  },
+  {
+    "revision": "267760da0febae96b7f5",
+    "url": "./static/js/main.d2af274d.chunk.js"
+  },
+  {
+    "revision": "e0f2a1c6b7945e38471c",
+    "url": "./static/js/runtime-main.57974a47.js"
+  }
+]);
